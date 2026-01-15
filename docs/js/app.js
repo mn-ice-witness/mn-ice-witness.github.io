cbd235dddc087b90e314e68d558a36a0ea55a31e
mn-ice-witness <mnicewitness@gmail.com>
2026-01-14 18:36:41 -0600

    Add 200ms delay before photos appear in splash

--- docs/js/app.js
+++ docs/js/app.js
@@ -47,7 +47,7 @@ const App = {
                 el.style.animation = '';
             });
             splash.classList.remove('hidden');
-            setTimeout(closeSplash, 3000);
+            setTimeout(closeSplash, 3200);
         };
 
         const titleLink = document.getElementById('title-link');
@@ -69,7 +69,7 @@ const App = {
 
         localStorage.setItem('splashLastShown', now.toString());
 
-        setTimeout(closeSplash, 3000);
+        setTimeout(closeSplash, 3200);
 
         splash.addEventListener('click', closeSplash);
 
