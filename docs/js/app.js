b28bd782f52c279ad8ec4f04979e7703a2cc35d3
mn-ice-witness <mnicewitness@gmail.com>
2026-01-14 18:35:30 -0600

    Slow down splash screen to 3 seconds

--- docs/js/app.js
+++ docs/js/app.js
@@ -47,7 +47,7 @@ const App = {
                 el.style.animation = '';
             });
             splash.classList.remove('hidden');
-            setTimeout(closeSplash, 2800);
+            setTimeout(closeSplash, 3000);
         };
 
         const titleLink = document.getElementById('title-link');
@@ -69,7 +69,7 @@ const App = {
 
         localStorage.setItem('splashLastShown', now.toString());
 
-        setTimeout(closeSplash, 2800);
+        setTimeout(closeSplash, 3000);
 
         splash.addEventListener('click', closeSplash);
 
